b11dedf836e675205b2c9f8e566ff575099fe2db
cjlofstrom <cjlofstrom@gmail.com>
2024-10-24 17:45:10 +0200

    styling

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -5,33 +5,23 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = React.useState(false)
-
-  React.useEffect(() => {
-    setMounted(true)
-  }, [])
-
-  if (!mounted) {
-    return null
-  }
-
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
 // src/components/providers/index.tsx
 "use client"
 
-import { ThemeProvider } from "./theme-provider"
+import { ThemeProvider as CustomThemeProvider } from "./theme-provider"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
+    <CustomThemeProvider
       attribute="class"
       defaultTheme="system"
       enableSystem
       disableTransitionOnChange
     >
       {children}
-    </ThemeProvider>
+    </CustomThemeProvider>
   )
 }
